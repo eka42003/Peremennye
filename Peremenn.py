17c04d804f88237eb3615f70d5291a97bbd31798
eka42003 <eka42003@yahoo.es>
2024-06-07 16:56:03 +0300

    Add files via upload

---
+++ Peremenn.py
@@ -0,0 +1,5 @@
+numero_tareas = 12.0
+minutos = 90.0
+nombre_del_curso = "Pyton"
+mediotiempo = (minutos//numero_tareas)
+print("Курс: ", nombre_del_curso,",", "всего задач: ", numero_tareas,",", "среднее время выполнения: ", mediotiempo , "минут") #надо ли деление заключать в скобки?
