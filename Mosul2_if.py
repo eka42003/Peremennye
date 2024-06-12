55820f0cbaa404c7463a807874a93ac13b916948
eka42003 <eka42003@yahoo.es>
2024-06-12 16:28:14 +0300

    Add files via upload

---
+++ Mosul2_if.py
@@ -0,0 +1,27 @@
+first=64
+second=65
+third=75
+if first==second and second==third:
+    print(3)
+elif first==second or second==third or first== third:
+    print (2)
+else:
+    print(0)
+first=65
+second=65
+third=75
+if first==second and second==third:
+    print(3)
+elif first==second or second==third or first== third:
+    print (2)
+else:
+    print(0)
+first=65
+second=65
+third=65
+if first==second and second==third:
+    print(3)
+elif first==second or second==third or first== third:
+    print (2)
+else:
+    print(0)
