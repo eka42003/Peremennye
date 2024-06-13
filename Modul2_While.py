79de67b68c8f686543bdaa69ae1b7ad91fa68904
eka42003 <eka42003@yahoo.es>
2024-06-13 17:23:13 +0300

    Add files via upload

---
+++ Modul2_While.py
@@ -0,0 +1,15 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+poor = int (0)
+while poor < len(my_list):
+    if my_list[poor] >= 0:
+        print(my_list[poor])
+        poor = poor + 1
+    else:
+       break
+
+
+
+
+
+
