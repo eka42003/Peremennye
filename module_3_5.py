075a0f32c6b6621e9c9b7f85d5c6389227f46ed3
eka42003 <eka42003@yahoo.es>
2024-07-07 12:18:52 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+def get_multiplied_digits(number):
+
+    str_number = str(number)
+    first = int(str_number[0])
+
+    n = len(str_number)
+
+    if n == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+
+print(get_multiplied_digits(10228))
