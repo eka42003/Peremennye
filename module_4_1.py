dd45568b31edc09578632be86a83919c99d3940f
eka42003 <eka42003@yahoo.es>
2024-07-11 12:41:19 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -4,4 +4,4 @@ import true_math as tm
 result_1 = print(fm.divide(65,3))
 result_2 = print(fm.divide(65,0))
 result_3 = print(tm.divide(65,3))
-result_2 = print(tm.divide(65,0))
+result_4 = print(tm.divide(65,0))
