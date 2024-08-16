a87f15df47022ef205f1713d46b2d5ad0c9fc2a8
eka42003 <eka42003@yahoo.es>
2024-08-16 14:23:03 +0300

    Update Sredny.py

--- Sredny.py
+++ Sredny.py
@@ -2,7 +2,7 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = list(students)
 students.sort()
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-for i in range(5):
+for i in range(len(grades):
     grades[i] = sum(grades[i]) / len(grades[i])
 grudents = dict(zip(students, grades))
 print(grudents)
