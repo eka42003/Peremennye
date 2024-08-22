8afab46dfe3a5d180e035ef6feb94d8b8e7c6b62
eka42003 <eka42003@yahoo.es>
2024-08-22 12:14:13 +0300

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,26 @@
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum(structure):
+    count_num = 0
+    count_str = 0
+
+    if isinstance(structure, int):
+        count_num += structure
+    elif isinstance (structure, str):
+        count_str += len(structure)
+    elif isinstance (structure, (list, tuple, set)):
+        for item in structure:
+            count_num += calculate_structure_sum(item)
+    elif isinstance(structure, dict):
+        for key, value in structure.items():
+            count_str += calculate_structure_sum(key)
+            count_num += calculate_structure_sum(value)
+    return count_num + count_str
+
+print(calculate_structure_sum(data_structure ))
